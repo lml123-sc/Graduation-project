3f70b8e68900e5a5f9306b4e125300716a446db4
耶鲁龙 <153621537+lml123-sc@users.noreply.github.com>
2024-02-28 10:24:57 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import "tailwindcss/tailwind.css"
+
+function App() {
+  return (
+    <div className="App">
+     < header className="App-header">
+        <span className="text-red-800">22</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
